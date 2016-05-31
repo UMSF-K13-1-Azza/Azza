e087272bed30754b3d2437fe21789b15f70e5a29
UMSF-K13-1-Azza <Vadim-vad@list.ru>
2016-05-31 12:37:38 +0300

    initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string patternlogin="^[a-z]+$";
+            string patternpassword = "^[0-9]+$";
+            while (true)
+            {
+                Console.Write("Enter login:");
+                string login = Console.ReadLine();
+                Console.Write("Enter password:");
+                string password = Console.ReadLine();
+                if ((Regex.IsMatch(login,patternlogin)) && (Regex.IsMatch(password,patternpassword)))
+                {
+                    Console.WriteLine("Registration successful!");
+                    break;
+                }
+                else Console.WriteLine("Wrong input!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
